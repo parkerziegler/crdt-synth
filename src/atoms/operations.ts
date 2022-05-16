e522ad28e27007526b5897b18646ae62e3f79cfb
parkerziegler <parkerellisziegler@gmail.com>
2022-05-16 15:08:58 -0700

    fix: unify operation conflict generation logic

--- src/atoms/operations.ts
+++ src/atoms/operations.ts
@@ -1,74 +1,118 @@
 import { atom } from "jotai";
 import zip from "lodash.zip";
 import shuffle from "lodash.shuffle";
+import random from "lodash.random";
 
-import { Op, OpType, OpTypeInt } from "../types/operations";
+import { Op, OpType, OpTypeInt, Replicas } from "../types/operations";
 
 const INITIAL_OPERATION_COUNT = 5;
+const MAX_OPERATION_PAYLOAD = 10;
 
-// Generate a random operation type for a given operation.
-const seedOperationType = () => {
+// Seed a conflicting pair of concurrent operations.
+const seedConflict = (): [OpType, OpType] => {
+  const flip = Math.random();
+
+  const op1Type = flip > 0.5 ? "add" : "rmv";
+  const op2Type = op1Type === "add" ? "rmv" : "add";
+
+  return [op1Type, op2Type];
+};
+
+// Seed a non-conflicting pair of concurrent operations.
+const seedNonConflict = (): [OpType, OpType] => {
   const flip = Math.random();
 
   if (flip < 0.33) {
-    return "add" as const;
+    const op2Type = Math.random() > 0.5 ? "add" : null;
+
+    return ["add", op2Type];
   } else if (flip < 0.66) {
-    return "rmv" as const;
+    const op2Type = Math.random() > 0.5 ? "rmv" : null;
+
+    return ["rmv", op2Type];
   } else {
-    return null;
+    const op2Type = Math.random() > 0.5 ? "add" : "rmv";
+
+    return [null, op2Type];
   }
 };
 
 // Generate an initial set of operations, ensuring that at least
 // one, but no more than 3, pairs of concurrent operations yield
 // a conflict.
-const generateOperations = ({ first }: { first: boolean }): Op[] => {
-  const numConflicts = Math.floor(Math.random() * (3 - 1) + 1);
+const generateOperations = (): Replicas => {
+  const replicas: Replicas = {
+    1: [],
+    2: [],
+  };
 
-  const conflictingOps = new Array(numConflicts).fill(undefined).map(() => {
-    // Ensure the two replicas always have differing types on conflict
-    // concurrent operations.
-    const firstType = Math.random() > 0.5 ? "add" : "rmv";
-    const secondType = firstType === "add" ? "rmv" : "add";
+  // Derive a random number of conflicts within the range [1, 3].
+  const numConflicts = random(1, 3);
+  const conflictPairs = new Array(numConflicts).fill(undefined).map(() => {
+    const [op1Type, op2Type] = seedConflict();
 
-    return {
-      type: first ? (firstType as OpType) : (secondType as OpType),
-      payload: Math.floor(Math.random() * 10),
+    const replica1Op = {
+      type: op1Type,
+      payload: Math.floor(Math.random() * MAX_OPERATION_PAYLOAD),
       reconciled: false,
-      first,
+      first: true,
     };
+
+    const replica2Op = {
+      type: op2Type,
+      payload: Math.floor(Math.random() * MAX_OPERATION_PAYLOAD),
+      reconciled: false,
+      first: false,
+    };
+
+    return [replica1Op, replica2Op];
   });
 
-  const nonConflictingOps = new Array(INITIAL_OPERATION_COUNT - numConflicts)
+  // Seed the remaining operations with random non-conflicts.
+  const numNonConflicts = INITIAL_OPERATION_COUNT - numConflicts;
+  const nonConflictPairs = new Array(numNonConflicts)
     .fill(undefined)
     .map(() => {
-      return {
-        type: seedOperationType(),
-        payload: Math.floor(Math.random() * 10),
+      const [op1Type, op2Type] = seedNonConflict();
+
+      const replica1Op = {
+        type: op1Type,
+        payload: Math.floor(Math.random() * MAX_OPERATION_PAYLOAD),
         reconciled: false,
-        first,
+        first: true,
       };
+
+      const replica2Op = {
+        type: op2Type,
+        payload: Math.floor(Math.random() * MAX_OPERATION_PAYLOAD),
+        reconciled: false,
+        first: false,
+      };
+
+      return [replica1Op, replica2Op];
     });
 
   // Shuffle the conflicting and non-conflicting operations.
-  return shuffle(
-    conflictingOps.concat(nonConflictingOps).map((op, i) => {
-      return {
-        ...op,
-        id: `operation_${i}`,
-      };
-    })
-  );
+  const pairs = shuffle(conflictPairs.concat(nonConflictPairs));
+
+  // Push the operations into their respective "replicas".
+  for (const [i, [op1, op2]] of pairs.entries()) {
+    replicas["1"].push({
+      ...op1,
+      id: `operation_${i}`,
+    });
+
+    replicas["2"].push({
+      ...op2,
+      id: `operation_${i}`,
+    });
+  }
+
+  return replicas;
 };
 
-const replica1InitialOperations = generateOperations({ first: true });
-const replica2InitialOperations = generateOperations({ first: false });
-
 // The central application state that is modified by user actions.
-export const operationsAtom = atom({
-  "1": replica1InitialOperations,
-  "2": replica2InitialOperations,
-});
+export const operationsAtom = atom(generateOperations());
 
 const OP_TYPE_TO_INT = {
   add: 1 as const,
